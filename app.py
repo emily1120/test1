dd83414932de58ad47cfc2190b57d7f247f8a2e6
emily1120 <168400249+emily1120@users.noreply.github.com>
2024-05-13 14:54:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if len(id_number)!=10:
     return "身分證號碼應該為10碼" , 400
 if not id_number[0].isalpha():
     return "第一個字元應該為英文字母碼" , 400
-if not id_number[1:].isdight():
+if not id_number[1:].isdigit():
     return "身分證號碼後九碼應為數字" , 400
 
 @app.route('/')
